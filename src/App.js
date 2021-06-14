7f8a32191d130dacdcccb85609c6da6360b2028a
jeahun10717 <jeahun10717@gmail.com>
2021-06-14 16:24:10 +0900

    ~4.4 movie_app_2019 upload

--- src/App.js
+++ src/App.js
@@ -1,37 +1,51 @@
 import React from "react";
 import axios from "axios";
-
 import Movie from "./Movie";
+import "./App.css";
 
-class App extends React.Component{
+class App extends React.Component {
   state = {
     isLoading: true,
     movies: []
   };
-  // getMovies = async () => {
-  //   const movie = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json")
-  //   console.log(movie);
-  //   console.log(movie.data.data.movies);
-  // }
   getMovies = async () => {
-    const {data: {data: {movies}}} = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating")
-    this.setState({movies, isLoading: false}) // {movies:movies} {moives} 는 완전히 같은 기능임
-  }
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+  };
   componentDidMount() {
     this.getMovies();
   }
-
-  render(){
-    const {isLoading, movies} = this.state;
-    return <div>{isLoading ? "Loading" : movies.map(movie=>(
-       <Movie
-      key={movie.id}
-      id={movie.id} 
-      year={movie.year} 
-      title={movie.title} 
-      summary={movie.summary} 
-      poster={movie.medium_cover_image}/>
-    ))}</div>
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
   }
 }
 
